a9948d9fe7338d529b12139f66b0a640b6e8c520
jonathanhsu0101 <jonathan.hsu0101@gmail.com>
2019-04-15 13:58:52 -0600

    It's working!

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#/"><h3>Full-Stack Picture-Message Board</h3><h5>by Hua-Ching Hsu</h5></a>
+    </nav>
+    <router-view class="container"/>
+  </div>
+</template>
+
+<style>
+</style>
